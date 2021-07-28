acf9f7652ebd4b88af76fde7cdc29b6e42b185fc
Roberto Miguel García Aguirre <61521501+RobertMike-10@users.noreply.github.com>
2021-07-28 12:09:50 -0500

    initial commit

---
+++ ConsoleAppTDD/TestProjectTDD/PasswordTests.cs
@@ -0,0 +1,58 @@
+﻿using ConsoleAppTDD;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+namespace TestProjectTDD
+{
+    public class PasswordTests
+    {
+        [Fact]
+        public void Validate_GivenLongerThan8Chars_ReturnsTrue()
+        {
+            var target = new DefaultPasswordValidator();
+            var password = MakeString(7) +"A";
+            var validation = target.Validate(password);
+            Assert.True(validation);
+        }
+
+        [Fact]
+        public void Validate_GivenShorterThan8Chars_ReturnsFalse()
+        {
+            var target = new DefaultPasswordValidator();
+            var password = MakeString(6) +"A";
+            var validation = target.Validate(password);
+            Assert.False(validation);
+        }
+
+        [Fact]
+        public void Validate_GivenNoUpperCase_ReturnsFalse()
+        {
+            var target = new DefaultPasswordValidator();
+            var password = MakeString(8);
+            var validation = target.Validate(password);
+            Assert.False(validation);
+        }
+
+        [Fact]
+        public void Validate_GivenOneUpperCase_ReturnsTrue()
+        {
+            var target = new DefaultPasswordValidator();
+            var password = MakeString(7) +"A";
+            var validation = target.Validate(password);
+            Assert.True(validation);
+        }
+
+        private string MakeString(int lenght)
+        {
+            string result = "";
+            for(int i=1;i<=lenght;i++)
+            {
+                result += "a";
+            }
+            return result;
+        }
+    }
+}
